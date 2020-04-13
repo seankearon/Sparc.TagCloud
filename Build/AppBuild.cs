f3635e1182108a5ad81e5d342f27107c3d55c127
seankearon <sean@tackaberry.org>
2020-04-13 11:15:05 +0100

    Added Nuke build to produce NuGet package.

---
+++ Build/AppBuild.cs
@@ -0,0 +1,88 @@
+using System;
+using Build;
+using Nuke.Common;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.Git;
+using Nuke.Common.Tools.NuGet;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[CheckBuildProjectConfigurations]
+[UnsetVisualStudioEnvironmentVariables]
+class AppBuild : NukeBuild
+{
+    public static int Main () => Execute<AppBuild>(x => x.Pack);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+    [PathExecutable] readonly Tool NuGet;
+
+    AbsolutePath PackageOutputDirectory => RootDirectory / "output";
+    AbsolutePath TestProject => RootDirectory / "TagCloud.Core.Tests" / "TagCloud.Core.Tests.csproj";
+    Project TagCloudCoreProject => Solution.GetProject("TagCloud.Core");
+    AbsolutePath TagCloudCoreOutputPath => RootDirectory / "TagCloud.Core" / "bin" / Configuration / "netstandard2.0";
+    Version NextVersion => TagCloudCoreProject.GetNextVersion();
+    readonly string NuspecFileName = "Sparc.TagCloud.Core.nuspec";
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            //if (!GitTasks.GitHasCleanWorkingCopy()) ControlFlow.Fail("Git does not have a clean working copy.");
+            EnsureCleanDirectory(PackageOutputDirectory);
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(s => s.SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+       .DependsOn(Restore)
+       .Executes(() =>
+        {
+            var version = NextVersion;
+            TagCloudCoreProject.TagRepository(version);
+
+            DotNetBuild(
+                s => s
+                   .SetProjectFile(Solution)
+                   .SetConfiguration(Configuration)
+                   .SetAssemblyVersion(version.ToString())
+                   .SetFileVersion(version.ToString())
+                   .SetInformationalVersion(version.ToString())
+                   .EnableNoRestore());
+        });
+
+    Target Test => _ => _
+       .DependsOn(Compile)
+       .Executes(() =>
+        {
+            DotNetTest(s => s.SetProjectFile(TestProject));
+        });
+
+    Target Pack => _ => _
+       .DependsOn(Compile, Test)
+       .Executes(() =>
+        {
+            NuGetTasks.NuGetPack(
+                s => s
+                   .SetTargetPath(NuspecFileName)
+                   .DisableBuild()
+                   .SetWorkingDirectory(TagCloudCoreOutputPath)
+                   .SetConfiguration(Configuration)
+                   .SetVersion(NextVersion.ThreeString())
+                   .SetOutputDirectory(PackageOutputDirectory)
+            );
+            TagCloudCoreProject.SaveAndUpdateVersionFile(NextVersion);
+        });
+}
--- Build/Extensions.cs
+++ Build/Extensions.cs
@@ -1,7 +1,42 @@
-﻿namespace Build
+﻿using System;
+using System.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.Git;
+
+namespace Build
 {
-    public class Extensions
+    public static class Extensions
     {
-        
-    }
-}
+        public static string ReadText(this    string  filePath)                   => File.ReadAllText(filePath);
+        public static void   SaveText(this    string  filePath, string  contents) => File.WriteAllText(filePath, contents);
+        public static void   SaveVersion(this Project project,  Version version)  => project.VersionFilePath().SaveText(version.ThreeString());
+
+        public static string VersionFilePath(this Project project)
+        {
+            return project.Directory / $"Ver.{project.Name.ToLowerInvariant()}.txt";
+        }
+
+        public static string ThreeString(this Version version) => $"{version.Major}.{version.Minor}.{version.Build}";
+
+        public static Project TagRepository(this Project project, Version version)
+        {
+            GitTasks.Git($"tag {project.Name}-{version.ThreeString()}");
+            return project;
+        }
+
+        public static Project SaveAndUpdateVersionFile(this Project project, Version version)
+        {
+            project.SaveVersion(version);
+            GitTasks.Git($"stage {project.VersionFilePath()}");
+            GitTasks.Git("commit -m \"Version number updated by the build.\" ");
+            // GitTasks.Git("push");
+            return project;
+        }
+
+        public static Version GetNextVersion(this Project project)
+        {
+            var currentVersionString = project.VersionFilePath().ReadText();
+            var version              = Version.Parse(currentVersionString);
+            return new Version(version.Major, version.Minor, version.Build + 1);
+        }
+    }}
